757734f70c6aa706d0b4cc6b783dd42c5c03047f
Peter <diosdadopeter@gmail.com>
2023-08-16 12:16:55 -0500

    feat: deleted assets, made card component w/styles

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+function Button( props:
+    {buttonText: string}
+) {
+  return (
+    <button>{props.buttonText}</button>
+  )
+}
+
+export default Button
---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="card">
+        <div className="text-container">
+            <h3>Todo Item</h3>
+            <p>peter diosdado be coding</p>
+        </div>
+        <div className="checkbox-container">
+            <input type="checkbox" />
+        </div>
+    </div>
+  )
+}
+
+export default Card
