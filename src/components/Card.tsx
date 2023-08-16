19ed5c8f2b749d132bbc7399b3d0b6b1a3ebfb1c
Peter <diosdadopeter@gmail.com>
2023-08-16 13:24:14 -0500

    style: css for text overflow & darkmode for future

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,7 @@ function Card() {
   return (
     <div className="card">
         <div className="text-container">
-            <h3>Todo Item</h3>
-            <p>peter diosdado be coding</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div className="checkbox-container">
             <input type="checkbox" />
